8b7a697a573fa52edeac325fedd0e6a42bcbea58
marceloaaps <marceloalexandreaps@gmail.com>
2025-01-17 13:40:26 -0300

    Separação classes Mapper e alteração porta de acesso.

--- src/main/java/com/marcelospring/forumhub/infra/adapters/mappers/TopicoMapper.java
+++ src/main/java/com/marcelospring/forumhub/infra/adapters/mappers/TopicoMapper.java
@@ -14,6 +14,5 @@ public interface TopicoMapper {
 
     Topico toEntity(TopicoDto topicoDto);
 
-    Usuario toEntity(UsuarioDto usuarioDto);
 
 }
---
+++ src/main/java/com/marcelospring/forumhub/infra/adapters/mappers/UsuarioMapper.java
@@ -0,0 +1,14 @@
+package com.marcelospring.forumhub.infra.adapters.mappers;
+
+import com.marcelospring.forumhub.core.domain.entities.Usuario;
+import com.marcelospring.forumhub.presentation.dtos.UsuarioDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UsuarioMapper {
+
+    UsuarioMapper INSTANCE = Mappers.getMapper(UsuarioMapper.class);
+    Usuario toEntity(UsuarioDto usuarioDto);
+
+}
