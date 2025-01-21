154cfb9845c4f4d98b711633a251bbb317857ef7
marceloaaps <marceloalexandreaps@gmail.com>
2025-01-21 18:10:10 -0300

    Fixes, está completamente funcional.

--- src/main/java/com/marcelospring/forumhub/presentation/dtos/UsuarioDto.java
+++ src/main/java/com/marcelospring/forumhub/presentation/dtos/UsuarioDto.java
@@ -2,9 +2,6 @@ package com.marcelospring.forumhub.presentation.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 
-import java.io.Serializable;
-import java.util.List;
-
 public record UsuarioDto(Long id,
                          @NotBlank String nome, String email, String senha){
 }
